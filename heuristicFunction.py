de51e6b19f6b6cbcc4ff795ddaece87664c4463a
MohyEldeen <149064360+mohymohysaleh@users.noreply.github.com>
2024-11-16 12:06:44 +0200

    Update heuristicFunction.py

--- heuristicFunction.py
+++ heuristicFunction.py
@@ -1,6 +1,6 @@
-def heuristic(current, goal):
+def heuristic_2(current, goal):
     return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
 
-def heuristic_2(a, b):
+def heuristic(a, b):
      return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
